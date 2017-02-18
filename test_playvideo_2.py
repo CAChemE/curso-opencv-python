b432dcaeee644df0cb063a39dfd80b57a587dabb
Fran Navarro <fjnavarrob@gmail.com>
2017-02-18 19:51:08 +0100

    added some logic to avoid errors when video ends

--- test_playvideo_2.py
+++ test_playvideo_2.py
@@ -3,19 +3,25 @@ import cv2
 
 cap = cv2.VideoCapture('samples/data/vtest.avi')
 
-print(cap.read())
+_, frame = cap.read()
+
+assert frame is not None, "Fmmpeg seems not to be installed properly"
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-
-    fgmask = fgbg.apply(frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 	
-    cv2.imshow('frame',fgmask)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret == True:
+
+        fgmask = fgbg.apply(frame)
+        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+		
+        cv2.imshow('frame',fgmask)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else: 
         break
 
 cap.release()
