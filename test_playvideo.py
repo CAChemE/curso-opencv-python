c0a7219e3260e728ee5fbf6370fd3f90afbd3889
Fran Navarro <fjnavarrob@gmail.com>
2017-02-18 19:48:23 +0100

    added some logic to avoid error ar the end of the video

--- test_playvideo.py
+++ test_playvideo.py
@@ -3,16 +3,22 @@ import cv2
 
 cap = cv2.VideoCapture('samples/data/vtest.avi')
 
-print(cap.read())
+_, frame = cap.read()
+
+assert frame is not None, "Fmmpeg seems not to be installed properly"
 
 while(cap.isOpened()):
-    ret, frame = cap.read()
+	ret, frame = cap.read()
+	if ret == True:
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+		cv2.imshow('frame',gray)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	else:
+		break
+		
 
 cap.release()
 cv2.destroyAllWindows()
